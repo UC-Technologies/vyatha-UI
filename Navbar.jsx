d0cb94a35118eaeac1f95a08bb3b5a6be670f0b0
Pratyush-Kumar-224 <139564132+Pratyush-Kumar-224@users.noreply.github.com>
2023-09-25 21:56:06 +0530

    Add files via upload

---
+++ Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <main>
+        {/* Your page content here */}
+      </main>
+    </div>
+  );
+}
