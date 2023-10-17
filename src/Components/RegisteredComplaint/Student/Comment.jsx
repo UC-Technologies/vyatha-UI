a0cf3ca1e22604ecece63efbcc58247fbbef857d
Rishabh Ranjan <ranjan.rishabh0902@gmail.com>
2023-10-17 18:20:52 +0530

    Started working on cloned repository

--- src/Components/RegisteredComplaint/Student/Comment.jsx
+++ src/Components/RegisteredComplaint/Student/Comment.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import styles from "./Comment.module.scss";
 
 const Comment = () => {
-  return <div>Comment</div>;
+  return (
+    <div className={styles.comments}>
+      <input type="text" placeholder="Tap to comment" />
+      <h1 className={styles.RaiseComplain}>Raise Complain</h1>
+
+      <div className={styles.dropdown}>
+        <div className={styles.select}>
+          <span className={styles.selected}>Warden</span>
+        </div>
+      </div>
+      <ul className={styles.menu}>
+        <li className={styles.active}>Warden</li>
+        <li>Dean</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Comment;
--- src/Pages/Auth/Student/Signup/StudentSignup.jsx
+++ src/Pages/Auth/Student/Signup/StudentSignup.jsx
@@ -1,12 +1,54 @@
-import React from "react";
+import styles from "./StudentSignup.module.scss";
 
-const StudentSignup = () => {
+const StudentSignUp = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      StudentSignup
-      {/* Signup for students page code comes here */}
-    </div>
+    <>
+      <h1>Sign Up</h1>
+      <form action="">
+        <div className={styles.form}>
+          <input type="text" placeholder=" " className={styles.nameinput} id="name" />
+          <label htmlFor="name">Name</label>
+        </div>
+        <div className={styles.form}>
+          <input type="email" placeholder=" " className={styles.nameinput} id="email" />
+          <label htmlFor="email">Email</label>
+        </div>
+        <div className={styles.form}>
+          <input
+            type="number"
+            placeholder=" "
+            className={styles.nameinput}
+            id="scholar"
+          />
+          <label htmlFor="scholar">Scholar ID</label>
+        </div>
+        <div className={styles.form}>
+          <input type="number" placeholder=" " className={styles.nameinput} id="phone" />
+          <label htmlFor="phone">Phone</label>
+        </div>
+        <div className={styles.form}>
+          <input type="password" placeholder=" " className={styles.nameinput} id="pass" />
+          <label htmlFor="pass">Password</label>
+        </div>
+        <div className={styles.form}>
+          <input
+            type="password"
+            placeholder=" "
+            className={styles.nameinput}
+            id="passconf"
+          />
+          <label htmlFor="passconf">Confirm Password</label>
+        </div>
+
+        <button type="submit" onClick={handleSubmit}>
+          Submit
+        </button>
+      </form>
+    </>
   );
 };
 
-export default StudentSignup;
+export default StudentSignUp;
