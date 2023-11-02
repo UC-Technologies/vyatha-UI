6dc1d8ca886b48fd4ab9826fa6d87c78bef5cb31
Nandini <th.nandini04@gmail.com>
2023-11-02 23:10:54 +0530

    ADD: About us page

--- src/Pages/Auth/Student/Login/StudentLogin.jsx
+++ src/Pages/Auth/Student/Login/StudentLogin.jsx
@@ -1,12 +1,47 @@
 import React from "react";
-
+import Captcha from "../../../../Components/Shared/CaptchaComponent/Captcha";
+import styles from "./StudentLogin.module.scss";
 const StudentLogin = () => {
+  const verifyCaptcha = () => {};
   return (
-    <div>
-      StudentLogin
-      {/* login for students page code comes here */}
+    <div className={styles.container}>
+      <p className={styles.login}>Log in</p>
+      <form className={styles.form_group}>
+        <div className={styles.input_group}>
+          <input
+            type="number"
+            id="number"
+            className={styles.form_input}
+            placeholder=" "
+          ></input>
+          <label className={styles.form_label} htmlFor="number">
+            Scholarid
+          </label>
+        </div>
+        <div className={styles.input_group}>
+          <input
+            type="password"
+            id="password"
+            className={styles.form_input}
+            placeholder=" "
+          ></input>
+          <label className={styles.form_label} htmlFor="password">
+            password
+          </label>
+        </div>
+        {/* <Captcha/> */}
+        <p className={styles.password}>Forgot Password?</p>
+        <div className={styles.captcha}>
+          <Captcha />
+        </div>
+        <div className={styles.button}>
+          <button type="submit" onClick={verifyCaptcha} className={styles.btn}>
+            Login
+          </button>
+        </div>
+        <p className={styles.signup}>Don&apos;t have an account?Sign up</p>
+      </form>
     </div>
   );
 };
-
 export default StudentLogin;
