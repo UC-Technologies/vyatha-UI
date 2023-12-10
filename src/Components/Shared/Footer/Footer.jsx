5124572bd0a8f4d16b118a786f5ac642390891dd
Pratyush Kumar <139564132+Pratyush-Kumar-224@users.noreply.github.com>
2023-12-10 12:14:08 +0530

    Update Footer.jsx
    footer.jsx file is updated

--- src/Components/Shared/Footer/Footer.jsx
+++ src/Components/Shared/Footer/Footer.jsx
@@ -1,7 +1,34 @@
-// import React from 'react'
+import React from 'react'
+import './Footer.module.scss';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+      <h1 className="navbar-logo">Logo</h1>
+        <ul>
+          <li><b>Vyatha</b>qazwsxedc rfvtgbyhn ujmikol p</li>
+          <li><b>Email</b></li>
+          <li><u>uclimited234@gmail.com</u></li>
+          <li><b>Address</b></li>
+          <li>NIT Silchar, Srijan E-Cell, 788010</li>
+          <li><b>Phone</b></li>
+          <li>+91 7165471781</li>
+        </ul>
+      </div>
+      <div className="social-handles">
+        <h6>Follow Us</h6>
+        <div className="social-bg">
+        <ul>
+          <li><a href="#"><img src="" alt="Linkedin-handle" /></a></li>
+          <li><a href="#"><img src="" alt="Facebook-handle" /></a></li>
+          <li><a href="#"><img src="" alt="X-handle" /></a></li>
+        </ul>
+        <p>Copyright ucvyatha@ All rights protected</p>
+        </div>  
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
