a61e293d80b7aa08a0f8fa907ccc07037af53573
Pratyush Kumar <139564132+Pratyush-Kumar-224@users.noreply.github.com>
2023-12-10 12:30:40 +0530

    Update Navbar.jsx
    navbar.jsx file is updated

--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -1,13 +1,30 @@
-// eslint-disable-next-line import/no-unresolved
-// import Vite from './public/vite.svg'
-import styles from "./Navbar.module.scss";
+import { useState } from 'react';
+import './Navbar.module.scss';
 
-const Navbar = () => {
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <h1 className={styles.myh1}>
-      <img src="/vite.svg" alt="" />
-    </h1>
+    <nav className={`navbar ${isOpen ? 'open' : ''}`}>
+      <div className="navbar-container">
+        <h1 className="navbar-logo">Logo</h1>
+        <button onClick={toggleNavbar} className="navbar-menu-button">
+          &#9776;
+        </button>
+        <ul className={`navbar-nav-links ${isOpen ? 'open' : ''}`}>
+          <li><a href="#"><img src="" alt="My Profile"></img><p>My Profile</p></a></li>
+          <li><a href="#"><img src="" alt="Register complaints"></img><p>Register complaints</p></a></li>
+          <li><a href="#"><img src="" alt="Registered complaints"></img><p>Registered complaints</p></a></li>
+          <li><a href="#"><img src="" alt="About Us"></img><p>About Us</p></a></li>
+          <li><a href="#"><img src="" alt="Log Out"></img><p>Log Out</p></a></li>
+        </ul>
+      </div>
+    </nav>
   );
-};
+}
 
 export default Navbar;
