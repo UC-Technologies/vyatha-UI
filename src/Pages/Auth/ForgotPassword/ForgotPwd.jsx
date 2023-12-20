f59e289001a69023775b09a055415b4339bd817c
Priyanka <mandirakumari1538@gmail.com>
2023-12-20 11:33:52 +0530

    Forget and Reset password pages created

--- src/Pages/Auth/ForgotPassword/ForgotPwd.jsx
+++ src/Pages/Auth/ForgotPassword/ForgotPwd.jsx
@@ -1,14 +1,24 @@
 import React, { useEffect } from "react";
-
+import styles from "./ForgotPwd.module.scss"
 const ForgotPwd = () => {
   useEffect(() => {
     document.title = "Forgot Password?";
   }, []);
   return (
-    <div>
-      {/* it will have a from that will ask for email */}
-      ForgotPwd
-    </div>
+    <div className={styles.Forget}>
+    <div className={styles.forgot_page}>
+<div className={styles.forget_title}>Forgot Password</div>
+<form action="">
+<div className={styles.form}>
+  <input type="email" placeholder="" className={styles.nameinput} id="email" />
+  <label htmlFor="email">Email</label>
+</div>
+<button type="submit" >
+  Confirm
+</button>
+</form>
+</div>
+ </div>
   );
 };
 
--- src/Pages/Auth/ForgotPassword/ResetPwd.jsx
+++ src/Pages/Auth/ForgotPassword/ResetPwd.jsx
@@ -1,14 +1,31 @@
 import React, { useEffect } from "react";
-
+import styles from "./ResetPwd.module.scss"
 const ResetPwd = () => {
   useEffect(() => {
     document.title = "Reset Password";
   }, []);
   return (
-    <div>
-      {/* there will be a form that will ask for new password */}
-      ResetPwd
-    </div>
+    <div className={styles.Reset}>
+            <div className={styles.reset_page}>
+        <div className={styles.reset_title}>Reset Password</div>
+        <form action="">
+            <div className={styles.all_inputs}>
+        <div className={styles.form}>
+          <input type="password" placeholder="" className={styles.nameinput} id="new_pass" />
+          <label htmlFor="new_pass">New password</label>
+          
+        </div>
+        <div className={styles.form}>
+        <input type="password" placeholder="" className={styles.nameinput} id="confirm_pass" />
+          <label htmlFor="confirm_pass">Confirm password</label>
+        </div>
+        </div>
+        <button type="submit" >
+          Confirm
+        </button>
+        </form>
+        </div>
+         </div>
   );
 };
 
