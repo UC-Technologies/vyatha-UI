886daf58a495b62c296ac31e467f2fbdb6a46f43
Himangshu-101 <himangshu21_ug@ece.nits.ac.in>
2023-12-25 12:20:37 +0530

    UPD: Document title for Team page

--- src/Pages/Team/Team.jsx
+++ src/Pages/Team/Team.jsx
@@ -1,9 +1,14 @@
+import { useEffect } from "react";
 import styles from "./Team.module.scss";
 import CardTeam from "../../Components/Team/CardTeam";
 import teamData from "../../Assets/Team/teamData.json";
 import profPhoto from "../../Assets/Team/profPhoto.png";
 
 const Team = () => {
+  useEffect(() => {
+    document.title = "Team | Vyatha";
+  });
+
   const head = {
     photo: profPhoto,
     name: "Manish Sarmah",
