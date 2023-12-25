eee44dc2b50b8cab0f7fafb56beef8694b423852
Himangshu-101 <himangshu21_ug@ece.nits.ac.in>
2023-12-25 12:14:40 +0530

    ADD: Team page

---
+++ src/Pages/Team/Team.jsx
@@ -0,0 +1,43 @@
+import styles from "./Team.module.scss";
+import CardTeam from "../../Components/Team/CardTeam";
+import teamData from "../../Assets/Team/teamData.json";
+import profPhoto from "../../Assets/Team/profPhoto.png";
+
+const Team = () => {
+  const head = {
+    photo: profPhoto,
+    name: "Manish Sarmah",
+    designation: "Lead Developer",
+    url: "vyatha-ui.onrender.com/",
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.mainwrapper}>
+        <div className={styles.head}>
+          <CardTeam
+            photo={head.photo}
+            name={head.name}
+            designation={head.designation}
+            url={head.url}
+          />
+        </div>
+        <div className={styles.devs}>
+          {teamData &&
+            teamData.map((item) => (
+              <div className={styles.subwrapper}>
+                <CardTeam
+                  photo={item.photo}
+                  name={item.name}
+                  designation={item.designation}
+                  url={item.url}
+                />
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Team;
