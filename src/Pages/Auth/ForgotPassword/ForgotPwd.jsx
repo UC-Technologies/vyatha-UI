4997e63e647e80ca332214f1e5ed455516727f5e
hereisadi <hereisadi@proton.me>
2023-12-27 14:41:19 +0530

    backend integration of reset and forgot password

--- src/Pages/Auth/ForgotPassword/ForgotPwd.jsx
+++ src/Pages/Auth/ForgotPassword/ForgotPwd.jsx
@@ -1,12 +1,68 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { toast } from "sonner";
 import styles from "./ForgotPwd.module.scss";
+import { UserContext } from "../../../Context/Provider";
+
 const ForgotPwd = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const navigate = useNavigate();
+  const { isLoggedIn } = useContext(UserContext);
+  useEffect(() => {
+    if (isLoggedIn === true) navigate("/dashboard");
+  }, [isLoggedIn, navigate]);
+
+  const [email, setEmail] = useState("");
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
   };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios
+        .post(`${import.meta.env.VITE_REACT_APP_API}/forgotpassword`, { email })
+        .then((res) => {
+          if (res.data.message === "Reset link sent to your email") {
+            toast("Reset link sent to your email");
+          }
+        });
+    } catch (err) {
+      if (err.response) {
+        switch (err.response.data.error) {
+          case "Error in generating token":
+            toast("Error in generating token");
+            break;
+          case "Email is required":
+            toast("Email is required");
+            break;
+          case "email is missing":
+            toast("email is missing");
+            break;
+
+          case "Email is not valid":
+            toast("Email is not valid");
+            break;
+          case "No account exists":
+            toast("No account exists");
+            break;
+
+          case "Internal Server Error":
+            toast("Internal Server Error");
+            break;
+          default:
+            toast("Error in generating token");
+        }
+      }
+    } finally {
+      setEmail("");
+    }
+  };
+
   useEffect(() => {
     document.title = "Forgot Password | Vyatha";
   }, []);
+
   return (
     <div className={styles.forget}>
       <div className={styles.forget_title}>
@@ -15,7 +71,14 @@ const ForgotPwd = () => {
 
       <div className={styles.form_starts}>
         <div className={styles.form}>
-          <input type="email" placeholder="" className={styles.nameinput} id="email" />
+          <input
+            type="email"
+            placeholder=""
+            value={email}
+            onChange={handleEmailChange}
+            className={styles.nameinput}
+            id="email"
+          />
           <label htmlFor="email">Email</label>
         </div>
 
--- src/Pages/Auth/ForgotPassword/ResetPwd.jsx
+++ src/Pages/Auth/ForgotPassword/ResetPwd.jsx
@@ -1,12 +1,72 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useContext } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "sonner";
+import axios from "axios";
 import styles from "./ResetPwd.module.scss";
+import { UserContext } from "../../../Context/Provider";
+
 const ResetPwd = () => {
-  const handleSubmit = (e) => {
+  const { token } = useParams();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirmpassword").value;
+    try {
+      await axios
+        .post(`${import.meta.env.VITE_REACT_APP_API}/resetpassword/${token}`, {
+          password,
+          confirmPassword,
+        })
+        .then((res) => {
+          if (res.data.message === "Password reset successfully") {
+            toast("Password reset successfully");
+            setTimeout(() => {
+              navigate("/auth/login");
+            }, [1500]);
+          }
+        });
+    } catch (error) {
+      if (error.response) {
+        switch (error.response.data.error) {
+          case "Payload missing":
+            toast("Payload missing");
+            break;
+          case "Password must be atleast 8 characters long":
+            toast("Password must be atleast 8 characters long");
+            break;
+          case "Passwords do not match":
+            toast("Passwords do not match");
+            break;
+          case "Invalid token, user not found":
+            toast("Invalid token, user not found");
+            break;
+          case "Token expired":
+            toast("Token expired");
+            break;
+          case "Internal Server Error":
+            toast("Internal Server Error");
+            break;
+          default:
+            toast("Something went wrong");
+        }
+      }
+    } finally {
+      document.getElementById("password").value = "";
+      document.getElementById("confirmpassword").value = "";
+    }
   };
+
   useEffect(() => {
     document.title = "Reset Password | Vyatha";
   }, []);
+
+  const navigate = useNavigate();
+  const { isLoggedIn } = useContext(UserContext);
+  useEffect(() => {
+    if (isLoggedIn === true) navigate("/dashboard");
+  }, [isLoggedIn, navigate]);
+
   return (
     <div className={styles.reset}>
       <div className={styles.reset_title}>
@@ -29,7 +89,7 @@ const ResetPwd = () => {
             type="password"
             placeholder=""
             className={styles.nameinput}
-            id="password"
+            id="confirmpassword"
           />
           <label htmlFor="password">Confirm New Password</label>
         </div>
