fc754b67937f8e23554042a7960f8283e561e2f5
satyaaa111 <juman60000@gmail.com>
2024-01-10 12:50:37 +0530

    Made sure that footer always stays at the bottom of the page

--- src/App.jsx
+++ src/App.jsx
@@ -36,74 +36,84 @@ import ClosedIssues from "./Pages/Dashboard/SuperAdmin/AllIssues/ClosedIssues";
 import ClosedIssue from "./Pages/Dashboard/AllRegisteredComplaints/ClosedIssue";
 // import HostelWise from "./Pages/Dashboard/SuperAdmin/AllIssues/AllHostel";
 // import { UserContext } from "./Context/Provider";
-
+import styles from "./app.module.scss";
 const App = () => {
   // const isLoggedIn = useContext(UserContext)
   return (
     <>
       <BrowserRouter>
         <Navbar />
-        <Scrolling>
+        <div className={styles.bodyy}>
           <div>
-            <Routes>
-              {/* {isLoggedIn ? (<h1>logged in</h1>) : (<p>Not Logged In</p>)} */}
-              <Route exact path="/" element={<Home />} />
-              {/* about us route */}
-              <Route exact path="/about" element={<About />} />
-              {/* first page to display login and signup buttons */}
-              <Route exact path="/auth" element={<Auth />} />
-              <Route exact path="/auth/login" element={<Login />} />
-              <Route exact path="/auth/signup" element={<Signup />} />
-              {/* verify email after signup */}
-              <Route exact path="/verifyemail/:token" element={<Verify />} />{" "}
-              {/* token */}
-              {/* dashboard will contain all regsitered complaints so no need to have all complaints page */}
-              <Route exact path="/dashboard" element={<Dashboard />} />
-              <Route exact path="/:role/allcomplaints" element={<AllComplaints />} />
-              {/* register complaint */}
-              <Route exact path="/register_complaint" element={<ComplaintForm />} />
-              {/* individual complaint */}
-              <Route
-                exact
-                path="/:role/complaint/:key"
-                element={<IndividualComplaint />}
-              />
-              {/* closed issue */}
-              <Route exact path="/:role/closedissues" element={<ClosedIssue />} />
-              {/* profile */}
-              <Route exact path="/profile" element={<Profile />} />
-              {/* edit profile */}
-              <Route exact path="/profile/edit" element={<EditProfile />} />
-              <Route exact path="/logout" element={<Logout />} />
-              {/* forgot password (it will ask for email)  */}
-              <Route exact path="/forgotpassword" element={<ForgotPwd />} />
-              {/*  after the user clicks on link sent on email for reseting password, it will ask for new password */}
-              <Route exact path="/resetpassword/:token" element={<ResetPwd />} />
-              {/* team page route */}
-              {/* <Route exact path="/team" element={<Team />} /> */}
-              {/*  SUPERADMIN ROUTES ONLY */}
-              <Route exact path="/superadmin/allsignups" element={<AllSignups />} />
-              <Route exact path="/superadmin/allissues" element={<AllIssues />} />
-              <Route exact path="/superadmin/hostelwise" element={<HostelWise />} />
-              <Route
-                exact
-                path="/superadmin/issues/:hostel"
-                element={<IndividualHostel />}
-              />
-              <Route
-                exact
-                path="/superadmin/issues/closed/:hostel"
-                element={<ClosedIssues />}
-              />
-              <Route exact path="/superadmin/issue/:_id" element={<IndividualIssue />} />
-              <Route exact path="/profile/:_id" element={<IndividualProfile />} />
-              {/* 404 route */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
+            <Scrolling>
+              <div className={styles.content}>
+                <Routes>
+                  {/* {isLoggedIn ? (<h1>logged in</h1>) : (<p>Not Logged In</p>)} */}
+                  <Route exact path="/" element={<Home />} />
+                  {/* about us route */}
+                  <Route exact path="/about" element={<About />} />
+                  {/* first page to display login and signup buttons */}
+                  <Route exact path="/auth" element={<Auth />} />
+                  <Route exact path="/auth/login" element={<Login />} />
+                  <Route exact path="/auth/signup" element={<Signup />} />
+                  {/* verify email after signup */}
+                  <Route exact path="/verifyemail/:token" element={<Verify />} />{" "}
+                  {/* token */}
+                  {/* dashboard will contain all regsitered complaints so no need to have all complaints page */}
+                  <Route exact path="/dashboard" element={<Dashboard />} />
+                  <Route exact path="/:role/allcomplaints" element={<AllComplaints />} />
+                  {/* register complaint */}
+                  <Route exact path="/register_complaint" element={<ComplaintForm />} />
+                  {/* individual complaint */}
+                  <Route
+                    exact
+                    path="/:role/complaint/:key"
+                    element={<IndividualComplaint />}
+                  />
+                  {/* closed issue */}
+                  <Route exact path="/:role/closedissues" element={<ClosedIssue />} />
+                  {/* profile */}
+                  <Route exact path="/profile" element={<Profile />} />
+                  {/* edit profile */}
+                  <Route exact path="/profile/edit" element={<EditProfile />} />
+                  <Route exact path="/logout" element={<Logout />} />
+                  {/* forgot password (it will ask for email)  */}
+                  <Route exact path="/forgotpassword" element={<ForgotPwd />} />
+                  {/*  after the user clicks on link sent on email for reseting password, it will ask for new password */}
+                  <Route exact path="/resetpassword/:token" element={<ResetPwd />} />
+                  {/* team page route */}
+                  {/* <Route exact path="/team" element={<Team />} /> */}
+                  {/*  SUPERADMIN ROUTES ONLY */}
+                  <Route exact path="/superadmin/allsignups" element={<AllSignups />} />
+                  <Route exact path="/superadmin/allissues" element={<AllIssues />} />
+                  <Route exact path="/superadmin/hostelwise" element={<HostelWise />} />
+                  <Route
+                    exact
+                    path="/superadmin/issues/:hostel"
+                    element={<IndividualHostel />}
+                  />
+                  <Route
+                    exact
+                    path="/superadmin/issues/closed/:hostel"
+                    element={<ClosedIssues />}
+                  />
+                  <Route
+                    exact
+                    path="/superadmin/issue/:_id"
+                    element={<IndividualIssue />}
+                  />
+                  <Route exact path="/profile/:_id" element={<IndividualProfile />} />
+                  {/* 404 route */}
+                  <Route path="*" element={<NotFound />} />
+                </Routes>
+              </div>
+            </Scrolling>
           </div>
-        </Scrolling>
 
-        <Footer />
+          <div className={styles.footer}>
+            <Footer />
+          </div>
+        </div>
       </BrowserRouter>
       <ToastContainer />
       <Toaster />
